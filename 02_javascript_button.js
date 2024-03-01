d73fd071c0a108817e083c014dcf84114c324078
alchemypls <alchemy838@gmail.com>
2024-02-29 20:16:08 -0800

    i made one thing and broke another...

---
+++ 02_javascript_button.js
@@ -0,0 +1,10 @@
+function light(sw) {
+    var pic;
+    if(sw == 0) {
+      pic = "assists\pic_bulboff.gif"
+    } else {
+      pic = "assists\lightbulb\pic_bulbon.gif"
+    }
+    document.getElementById('assists\pic_bulboff.gif').src = pic;
+  }
+  
